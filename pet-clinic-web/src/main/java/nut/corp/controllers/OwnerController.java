c90eaaa26a93a92e8187c4d73887bdc050244735
dmitriy <vdmitriy003@gmail.com>
2022-04-10 00:47:15 +0300

    feat: owner controller index

--- pet-clinic-web/src/main/java/nut/corp/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/nut/corp/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package nut.corp.controllers;
 
-import org.springframework.stereotype.Controller;
+import nut.corp.model.Owner;
+import nut.corp.services.OwnerService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Collection;
+
+@RequestMapping("/owners")
+@RestController
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping(value = "/owners", method = RequestMethod.GET)
-    public String index() {
-        return "Owner Page";
+    public Collection<Owner> index(Model model) {
+        return ownerService.findAll();
     }
 }
