97bf6772f946ba80e592c986b24ba97b076e593a
DmitriyV003 <vdmitriy003@gmail.com>
2022-04-09 11:54:34 +0300

    feat: add services interfaces

---
+++ pet-clinic-data/src/main/java/nut/corp/services/OwnerService.java
@@ -0,0 +1,16 @@
+package nut.corp.services;
+
+import nut.corp.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findById(Long id);
+
+    Owner findByLastName(String lastName);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/nut/corp/services/PetService.java
@@ -0,0 +1,13 @@
+package nut.corp.services;
+
+import nut.corp.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet owner);
+
+    Set<Pet> findAll();
+}
