65e807e18ab2d739f1339b17fa9afd58e2f609f0
dmitriy <vdmitriy003@gmail.com>
2022-04-16 21:13:59 +0300

    feat: Speciality model

--- pet-clinic-data/src/main/java/nut/corp/model/Vet.java
+++ pet-clinic-data/src/main/java/nut/corp/model/Vet.java
@@ -1,6 +1,18 @@
 package nut.corp.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
     }
