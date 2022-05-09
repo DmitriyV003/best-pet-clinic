378a48545f0cc9a1e01fbaa7020e1a831e88b854
dmitriy <vdmitriy003@gmail.com>
2022-05-09 14:54:55 +0300

    feat: vet service

---
+++ pet-clinic-data/src/main/java/nut/corp/services/map/jpa/VetServiceJpa.java
@@ -0,0 +1,43 @@
+package nut.corp.services.map.jpa;
+
+import nut.corp.model.Vet;
+import nut.corp.repositories.VetRepository;
+import nut.corp.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("spirndatajpa")
+public class VetServiceJpa implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetServiceJpa(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+}
