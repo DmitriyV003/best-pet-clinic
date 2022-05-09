b212217a338c0e5e766c8f7896a1178e0399106b
dmitriy <vdmitriy003@gmail.com>
2022-05-09 15:05:30 +0300

    feat: services

---
+++ pet-clinic-data/src/main/java/nut/corp/services/map/jpa/PetTypeServiceJpa.java
@@ -0,0 +1,44 @@
+package nut.corp.services.map.jpa;
+
+import nut.corp.model.PetType;
+import nut.corp.model.Vet;
+import nut.corp.repositories.PetTypeRepository;
+import nut.corp.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeServiceJpa implements PetTypeService {
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceJpa(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+}
